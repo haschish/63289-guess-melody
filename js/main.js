9e207d14c09802e15090027a50d9e80b50747e5c
haschish <haschish@gmail.com>
2018-08-08 13:37:05 +0500

    fixed eslint

--- js/main.js
+++ js/main.js
@@ -1,52 +1,53 @@
+'use strict';
 const KEY_CODE = {
   arrowLeft: 37,
   arrowRight: 39
 };
-const app = document.querySelector('.app');
-const main = app.querySelector('section.main');
-const screens = Array.from(document.querySelectorAll('template'));
+const app = document.querySelector(`.app`);
+const main = app.querySelector(`section.main`);
+const screens = Array.from(document.querySelectorAll(`template`));
 let currentScreenIndex;
 
 const setScreenById = (screenId) => {
-  const index = screens.findIndex(template => template.id === screenId);
+  const index = screens.findIndex((template) => template.id === screenId);
   if (index >= 0) {
     setScreen(index);
   }
-}
+};
 const setScreen = (index) => {
   const len = screens.length;
-  index = index%len;
+  index = index % len;
   if (index < 0) {
     index = len + index;
   }
-  main.innerHTML = '';
+  main.innerHTML = ``;
   main.appendChild(screens[index].content.cloneNode(true));
   currentScreenIndex = index;
-}
+};
 const nextScreen = () => {
   setScreen(currentScreenIndex + 1);
-}
+};
 const prevScreen = () => {
   setScreen(currentScreenIndex - 1);
-}
+};
 const createArrowButton = (textContent) => {
-  const button = document.createElement('button');
-  button.classList.add('arrows__btn');
+  const button = document.createElement(`button`);
+  button.classList.add(`arrows__btn`);
   button.textContent = textContent;
   return button;
-}
+};
 
-setScreenById('welcome');
-document.addEventListener('keydown', (e) => {
-  if (e.keyCode ===  KEY_CODE.arrowLeft) {
+setScreenById(`welcome`);
+document.addEventListener(`keydown`, (e) => {
+  if (e.keyCode === KEY_CODE.arrowLeft) {
     prevScreen();
-  } else if (e.keyCode ===  KEY_CODE.arrowRight) {
+  } else if (e.keyCode === KEY_CODE.arrowRight) {
     nextScreen();
   }
 });
 
-const arrows = document.createElement('div');
-arrows.classList.add('arrows__wrap');
+const arrows = document.createElement(`div`);
+arrows.classList.add(`arrows__wrap`);
 arrows.innerHTML = `
   <style>
     .arrows__wrap {
@@ -61,14 +62,12 @@ arrows.innerHTML = `
       padding: 5px 20px;
     }
   </style>`;
-const arrowLeft = createArrowButton('<-');
-arrowLeft.addEventListener('click', prevScreen);
+const arrowLeft = createArrowButton(`<-`);
+arrowLeft.addEventListener(`click`, prevScreen);
 
-const arrowRight = createArrowButton('->');
-arrowRight.addEventListener('click', nextScreen);
+const arrowRight = createArrowButton(`->`);
+arrowRight.addEventListener(`click`, nextScreen);
 
 arrows.appendChild(arrowLeft);
 arrows.appendChild(arrowRight);
 app.appendChild(arrows);
-
-
